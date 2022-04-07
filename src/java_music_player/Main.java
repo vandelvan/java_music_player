9e07a9a89851dbd9b730af304715c4ad50a65d7c
vandelvan <vandel_van@hotmail.com>
2022-04-06 20:21:56 -0500

    plays mp3s

--- src/java_music_player/Main.java
+++ src/java_music_player/Main.java
@@ -6,6 +6,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+import javax.swing.JButton;
+import javax.swing.JProgressBar;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.awt.event.ActionEvent;
 
 public class Main extends JFrame {
 
@@ -37,6 +55,54 @@ public class Main extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		File s = new File("/home/vandelvan/Music");
+		File[] songList = s.listFiles();
+		
+		JList songQueue = new JList(songList);
+		contentPane.add(songQueue, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.SOUTH);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnPlay = new JButton("New button");
+		btnPlay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+		        try(FileInputStream fis=new FileInputStream((File)songQueue.getSelectedValue())){
+		            Player player=new Player(fis);
+		            player.play();
+		        }catch (JavaLayerException e1) {
+		        	System.out.println( "No es un fichero de audio");
+		        }catch (IOException ex) {
+		        	System.out.println("");
+		        }
+			}
+		});
+		panel.add(btnPlay, BorderLayout.CENTER);
+		
+		JButton btnPrev = new JButton("New button");
+		btnPrev.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel.add(btnPrev, BorderLayout.WEST);
+		
+		JButton btnNext = new JButton("New button");
+		btnNext.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel.add(btnNext, BorderLayout.EAST);
+		
+		JProgressBar songProgressBar = new JProgressBar();
+		panel.add(songProgressBar, BorderLayout.NORTH);
+		
+		JLabel lblCancionesEnCola = new JLabel("Canciones en cola:");
+		contentPane.add(lblCancionesEnCola, BorderLayout.WEST);
+		
+		JLabel lblReproductorDeMusica = new JLabel("Reproductor de musica");
+		contentPane.add(lblReproductorDeMusica, BorderLayout.NORTH);
 	}
 
 }
